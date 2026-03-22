47428b84cf288f9ce333f46090c469a2c270d0f0
FlavioMatias <flaviomatiasnf@gmail.com>
2026-03-21 22:37:07 -0300

    feat: adciona business exception

---
+++ api/src/main/java/api/digital_wallet/shared/exceptions/BusinessException.java
@@ -0,0 +1,16 @@
+package api.digital_wallet.shared.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final String code;
+    private final HttpStatus status;
+
+    public BusinessException(String message, String code, HttpStatus status) {
+        super(message);
+        this.code = code;
+        this.status = status;
+    }
+}
