9d35cea3255d8ff95fe87a3190a4fc7af2f57d3c
FlavioMatias <flaviomatiasnf@gmail.com>
2026-04-16 22:00:50 -0300

    feat: adiciona ajustes na config de mensageria

--- api/src/main/java/api/digital_wallet/shared/config/RabbitConfig.java
+++ api/src/main/java/api/digital_wallet/shared/config/RabbitConfig.java
@@ -1,5 +1,6 @@
 package api.digital_wallet.shared.config;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -9,9 +10,58 @@ import tools.jackson.databind.json.JsonMapper;
 @Configuration
 public class RabbitConfig {
 
+    // Configurações da Fila Principal
+    public static final String USER_CREATED_QUEUE = "user.created.wallet-service";
+    public static final String USER_EXCHANGE = "user-events";
+    public static final String USER_CREATED_ROUTING_KEY = "UserCreatedEvent";
+
+    // Configurações de DLQ
+    public static final String WALLET_DLX = "wallet-service.dlx";
+    public static final String USER_CREATED_DLQ = "user.created.wallet-service.dlq";
+    public static final String DLQ_ROUTING_KEY = "dead-letter";
+
     @Bean
     public MessageConverter messageConverter(JsonMapper jsonMapper) {
-
         return new JacksonJsonMessageConverter(jsonMapper);
+    }
+
+
+    @Bean
+    public TopicExchange deadLetterExchange() {
+        return new TopicExchange(WALLET_DLX);
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return new Queue(USER_CREATED_DLQ, true);
+    }
+
+    @Bean
+    public Binding deadLetterBinding() {
+        return BindingBuilder.bind(deadLetterQueue())
+                .to(deadLetterExchange())
+                .with(DLQ_ROUTING_KEY);
+    }
+
+  
+
+    @Bean
+    public TopicExchange userExchange() {
+        return new TopicExchange(USER_EXCHANGE);
+    }
+
+    @Bean
+    public Queue userCreatedQueue() {
+        return QueueBuilder.durable(USER_CREATED_QUEUE)
+                .withArgument("x-dead-letter-exchange", WALLET_DLX)
+                .withArgument("x-dead-letter-routing-key", DLQ_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Binding userCreatedBinding(Queue userCreatedQueue, TopicExchange userExchange) {
+        return BindingBuilder.bind(userCreatedQueue)
+                .to(userExchange)
+                .with(USER_CREATED_ROUTING_KEY);
     }
 }
